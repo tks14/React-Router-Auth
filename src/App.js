71799c95cbcba2f492344101ed0c1ef047cebe40
vm <tks14@yahoo.com>
2020-09-09 14:51:53 -0400

    Final commit for tutorial prior to adding own firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 
-import React from "react";
+import React, {useState} from "react";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import PrivateRoute from './PrivateRoute';
 import Home from "./pages/Home";
 import Admin from "./pages/Admin";
+import Login from "./pages/Login";
+import Signup from './pages/Signup';
 import { AuthContext } from "./context/auth";
 
 function App(props) {
+
+  const existingTokens = JSON.parse(localStorage.getItem("tokens"));
+  const [authTokens, setAuthTokens] = useState(existingTokens);
+  
+  const setTokens = (data) => {
+    localStorage.setItem("tokens", JSON.stringify(data));
+    setAuthTokens(data);
+  }
   return (
-    <AuthContext.Provider value={false}>
+    <AuthContext.Provider value={{authTokens, setAuthTokens: setTokens}}>
       <Router>
         <div>
           <ul>
@@ -20,6 +30,8 @@ function App(props) {
             </li>
           </ul>
           <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
           <PrivateRoute path="/admin" component={Admin} />
         </div>
       </Router>
