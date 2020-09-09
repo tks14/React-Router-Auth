b1579aef8229e32cf0fa6eef575f5cf1f2f4304f
vm <tks14@yahoo.com>
2020-09-09 11:48:31 -0400

    Initial commit for tutorial project to learn advanced authentication with react

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import React from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Admin from './pages/Admin';
+
+function App(props) {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home Page</Link>
+          </li>
+          <li>
+            <Link to="/admin">Admin Page</Link>
+          </li>
+        </ul>
+        <Route exact path="/" component={Home} />
+        <Route path="/admin" component={Admin} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
