41951d8fa11e7d5470475c0692fc4b7e742a25cb
vm <tks14@yahoo.com>
2020-09-09 12:21:23 -0400

    Private route and AuthForm added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 
 import React from "react";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
-import Home from './pages/Home';
-import Admin from './pages/Admin';
+import PrivateRoute from './PrivateRoute';
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
+import { AuthContext } from "./context/auth";
 
 function App(props) {
   return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home Page</Link>
-          </li>
-          <li>
-            <Link to="/admin">Admin Page</Link>
-          </li>
-        </ul>
-        <Route exact path="/" component={Home} />
-        <Route path="/admin" component={Admin} />
-      </div>
-    </Router>
+    <AuthContext.Provider value={false}>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home Page</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin Page</Link>
+            </li>
+          </ul>
+          <Route exact path="/" component={Home} />
+          <PrivateRoute path="/admin" component={Admin} />
+        </div>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
